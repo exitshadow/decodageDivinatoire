0782ce681aced04b988a64de0146bfb1686290ff
CoolBoogie90 <98624859+coolboogie90@users.noreply.github.com>
2022-10-26 15:20:38 +0200

    Created shuffle and stack functions

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -4,19 +4,44 @@
 
 /*---------Main---------*/
 //Shuffle deck
-//Spread deck
-//Flip card
-var cards = document.querySelectorAll('.card');
+// var cardsSelection = document.querySelectorAll(".card");        // !!! only cards in card-selector
 
-[...cards].forEach((card) => {
+// [...cardsSelection].forEach((card) => {
+//     card.addEventListener("onLoad", function () {      // !!! when loading application.html
+//     for (let i = 0; i < card.length; i++) {
+//         setTimeout(() => {
+//             card[i].classList.add("card" + i);
+//         }, 
+//         i * 150);
+//     };
+// });
+
+// //Stack deck
+
+// [...cardsSelection].forEach((card) => {
+//     card.addEventListener('stackDeck', function () {        //!!! when done selecting
+//     for (let i = 0; i < card.length; i++) {
+//         setTimeout(() => {
+//             card[i].classList.remove("card" + i);
+//         },
+//         i * 150);
+//     };
+// });
+
+//Flip selected cards on click
+var flipCards = document.querySelectorAll('.card');
+
+[...flipCards].forEach((card) => {
     card.addEventListener('click', function () {
         card.classList.toggle('is-flipped');
     });
 });
 
+/*in style.css*/
+        // .card{transition: transform 1s;}
+        // .card.is-flipped{transform: translateX(-100%) rotateY(-180deg)}
+
 //Slide card into designated position
 //Reverse spread and pile deck
 
 /*---------Interpretation---------*/
-//Text div alpha 0>1 
-
