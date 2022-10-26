cd95938646b6f7f2caab8417bd0d3206bb382692
exitshadow <exertingshadows@gmail.com>
2022-10-26 14:59:51 +0200

    setup of card picker, working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,7 @@ let referenceDeck;  // injecter tous les arcanes depuis le json
 let shuffledDeck;   // contenant qui va recevoir toutes les cartes mélangées
 
 const MAXPICK = 4;  // nombre maximum de cartes choisies
-let cardsPicked;    // liste des cartes tirées par le jouer.
+let cardsPicked = [];    // liste des cartes tirées par le jouer.
                     // sert aussi de compteur pour tester contre MAXPICK
 
 let domCardsPickedNames;    // objet DOM  pour l’injection des cartes sélectionnées
@@ -18,13 +18,22 @@ referenceDeck = arcanae.arcane; // à remplacer avec un vrai lecteur JSON
 shuffledDeck = shuffle(referenceDeck);
 
 let selectionArea = document.getElementById("selection-area");
+let cardElements = [];
 for (let i = 0; i < shuffledDeck.length; i++) {
-    let card = document.createElement("button");
-    card.setAttribute("id", `card-${i}}`);
-    card.setAttribute("class", "cards-position");
-    card.setAttribute("value", `${shuffledDeck[i].number}`);
-    selectionArea.appendChild(card);
+    let cardElement = document.createElement("button");
+    cardElement.setAttribute("id", `card-${i}}`);
+    cardElement.setAttribute("class", "cards-position");
+    cardElement.setAttribute("value", `${i}`);
+    cardElements.push(cardElement);
+    selectionArea.appendChild(cardElement);
 }
+
+cardElements.forEach(function(i) {
+    i.addEventListener("click", function(e) {
+        pickCard(e.target.value);
+        //alert(e.target.value);
+    });
+});
 
 //#endregion
 
@@ -66,6 +75,14 @@ function shuffle(deck) {
         copy.splice(rng, 1);
     }
     return shuffled;
+}
+
+function pickCard(value) {
+    if (cardsPicked.length <= MAXPICK - 1) {
+        cardsPicked.push(shuffledDeck[value]);
+        cardElements[value].remove();
+        console.log(shuffledDeck[value]);
+    }
 }
 
 //#endregion
